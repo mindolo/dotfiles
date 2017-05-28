070d8dc0d977ca40a8e56bc045b66308d3431126
mindolo <self@mindolo.net>
2017-05-28 15:20:39 +0100

    KWM fixes

--- home/.kwm/profiles/laptop.sh
+++ home/.kwm/profiles/laptop.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-set -u
+set -ux
 
-sleep 1
-
+# retry until kwm is up
 kwmc display -f 0
+kwm_up=${?}
+until [ ${kwm_up} == 0 ] ; do
+   echo "waiting for kwm to start"
+   sleep 0.1
+   kwmc display -f 0
+   kwm_up=$?
+done
+
+# cycle all the spaces
 kwmc space -fExperimental 1; sleep 0.5
 kwmc space -fExperimental 2; sleep 0.5
 kwmc space -fExperimental 3; sleep 0.5
@@ -17,4 +25,5 @@ kwmc space -fExperimental 10; sleep 0.5
 kwmc space -fExperimental 11; sleep 0.5
 kwmc space -fExperimental 12; sleep 0.5
 
+# return to terminals
 kwmc space -fExperimental 1; sleep 0.5
