26b30f66e55e8773b40f5639cddca7831055e5c5
mindolo <mindolo@protonmail.com>
2018-05-19 19:08:08 +0200

    Cleanup

--- home/.bash/functions.sh
+++ home/.bash/functions.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 confpath=${HOME}/.bash
 
 ##########
@@ -13,7 +14,7 @@ active_list=${confpath}/active_mods
 
 is_mod_active(){
     module=$1
-    if $(grep -qx $module $active_list 2> /dev/null); then
+    if grep -qx "$module" "$active_list" 2> /dev/null; then
         return 0
     else
         return 1
@@ -22,7 +23,7 @@ is_mod_active(){
 
 is_mod_enabled(){
     module=$1
-    if $(grep -qx $module $enabled_list 2> /dev/null); then
+    if grep -qx "$module" "$enabled_list" 2> /dev/null; then
         return 0
     else
         return 1
@@ -31,7 +32,7 @@ is_mod_enabled(){
 
 is_mod_present(){
     module=$1
-    if [ -e $mod_path/$module ]; then
+    if [ -e "$mod_path"/"$module" ]; then
         return 0
     else
         return 1
@@ -41,17 +42,17 @@ is_mod_present(){
 #Enable module and write to $enabled_list
 bash_enable_mod() {
     module=$1
-    if is_mod_present $module; then
-        if is_mod_enabled $module; then
-            echo "$module already enabled"
+    if is_mod_present "$module"; then
+        if is_mod_enabled "$module"; then
+            echo "${module} already enabled"
             return 1
         else
-            echo $module >> $enabled_list
+            echo "$module" >> "$enabled_list"
             echo "$module enabled, it will be available on next startup"
             return 0
         fi
     else
-        echo "$module not present"
+        echo "${module} not present"
         return 2
     fi
 }
@@ -59,11 +60,11 @@ bash_enable_mod() {
 bash_disable_mod() {
     module=$1
     if is_mod_enabled "$module"; then
-        echo -e "/$module/d\nwq"|ex -s $enabled_list
-        echo "$module disabled, it won't be active on next startup"
+        echo -e "/$module/d\nwq"|ex -s "$enabled_list"
+        echo "${module} disabled, it won't be active on next startup"
         return 0
     else
-        echo "$module not enabled"
+        echo "${module} not enabled"
         return 1
     fi
 }
@@ -71,17 +72,17 @@ bash_disable_mod() {
 #Shows enabled mods
 bash_list_enabled_mods() {
     echo "Modules active:"
-    cat $enabled_list
+    cat "$enabled_list"
     return 0
 }
 
 #Load all the enabled modules
 bash_load_modules(){
     #cleanup $active_list
-    echo > $active_list
-    for mod in $(cat $enabled_list); do
-        source $mod_path/$mod
-        echo $mod >> $active_list
+    echo > "$active_list"
+    for mod in $(cat "$enabled_list"); do
+        source "$mod_path"/"$mod"
+        echo "$mod" >> "$active_list"
     done
     return 0
 }
@@ -90,18 +91,18 @@ bash_load_modules(){
 #Useful for testing
 bash_load_module(){
     module=$1
-    if is_module_present $module;then
-        source $module;
-        echo "$module active"
+    if is_module_present "$module";then
+        source "$module";
+        echo "${module} active"
         return 0
     else
-        echo "Couldn't find $module"
+        echo "Couldn't find ${module}"
         return 1
     fi
 }
 #Shows active mods
 bash_list_active_mods() {
     echo "Modules enabled:"
-    cat $active_list
+    cat "$active_list"
     return 0
 }
